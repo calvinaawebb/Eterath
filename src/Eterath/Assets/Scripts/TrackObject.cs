326d21775c06820774fc20bc840103f187fd0425
FluxChikin <calvinaawebb@gmail.com>
2023-12-10 22:47:01 -0700

    Weekly DevLog #2
    Started work on importing characters with IK and animations, making new movement controller that is an actual player controller. Smaller week this week as I had a competition to attend but still somewhat productive.

--- src/Eterath/Assets/Scripts/TrackObject.cs
+++ src/Eterath/Assets/Scripts/TrackObject.cs
@@ -8,19 +8,31 @@ public class TrackObject : MonoBehaviour
     public Vector3 direct;
     public Rigidbody m_Rigidbody;
     public float m_Thrust = 0.001f;
+    public Animator playerAnimator;
     // Start is called before the first frame update
     void Start()
     {
+        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
         originalRot = transform.eulerAngles;
         m_Rigidbody = transform.parent.gameObject.GetComponent<Rigidbody>();
+        m_Rigidbody.freezeRotation = true;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.eulerAngles = originalRot;
-        originalRot.y += Input.GetAxis("Horizontal");
+        originalRot.y += Input.GetAxis("Mouse X")*3;
         direct = new Vector3(transform.forward.x, 0 ,transform.forward.z);
         m_Rigidbody.AddForce(direct * m_Thrust * Input.GetAxis("Vertical"));
+        if (Input.GetKeyDown("space"))
+        {
+            playerAnimator.SetBool("walking", true);
+        }
+        else 
+        {
+            playerAnimator.SetBool("walking", false);
+        }
     }
 }
